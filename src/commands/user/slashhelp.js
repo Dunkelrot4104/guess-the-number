0e2974f9cbfa34231ae726676789641e35082c2e
Dunkelrot <115803205+Dunkelrot4104@users.noreply.github.com>
2023-01-17 10:35:31 -0500

    Create slashhelp.js

---
+++ src/commands/user/slashhelp.js
@@ -0,0 +1,49 @@
+{
+  "name": "help",
+  "description": "Shows all available commands",
+  "options": [
+    {
+      "type": 3,
+      "name": "command",
+      "description": "The command you want more information on",
+      "choices": [
+        {
+          "name": "setup",
+          "value": "help_setup"
+        },
+        {
+          "name": "start",
+          "value": "help_start"
+        },
+        {
+          "name": "hint",
+          "value": "help_hint"
+        },
+        {
+          "name": "end",
+          "value": "help_end"
+        },
+        {
+          "name": "game",
+          "value": "help_game"
+        },
+        {
+          "name": "info",
+          "value": "help_info"
+        },
+        {
+          "name": "lb",
+          "value": "help_lb"
+        },
+        {
+          "name": "premium",
+          "value": "help_premium"
+        },
+        {
+          "name": "invite",
+          "value": "help_invite"
+        }
+      ]
+    }
+  ]
+}
